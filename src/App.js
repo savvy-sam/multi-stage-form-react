e3522228e22fd7b2e5bb6953d85039e8d8d86555
Samson Chege <samchege09@gmail.com>
2023-02-01 00:35:02 -0500

    Intial App Setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,43 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AppProvider } from "./state";
+import { Contact } from "./Steps/Contact";
+import { Education } from "./Steps/Education";
+import { About } from "./Steps/About";
+import { Confirm } from "./Steps/Confirm";
+import { Stepper } from "./Steps/Stepper";
+
+export const App = () => {
+  return (
+    <AppProvider>
+      <Router>
+        <Stepper />
+        <Routes>
+          <Route path="/" element={<Contact />} />
+          <Route path="/education" element={<Education />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/confirm" element={<Confirm />} />
+        </Routes>
+      </Router>
+    </AppProvider>
+  );
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -23,3 +62,4 @@ function App() {
 }
 
 export default App;
+*/
--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
-import React from 'react';
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.scss";
+import { App } from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <div className="App">
+      <App />
+    </div>
+  </StrictMode>
+);
+
+
+
+
+
+
+
+/* import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -15,3 +37,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
