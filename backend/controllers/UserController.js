74231ced2e19c224dbefaf8d698ebc8afd57d137
Samson Chege <samchege09@gmail.com>
2023-02-04 01:17:10 -0500

    Made Routes More Descriptive

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -25,7 +25,7 @@ export const getUserById = async(req, res) =>{
 export const createUser = async(req, res) =>{
     try {
         await User.create(req.body);
-        res.status(201).json({msg: "User Created"});
+        res.status(202).json({msg: "User Created"});
     } catch (error) {
         console.log(error.message);
     }
--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -11,7 +11,7 @@ const router = express.Router();
  
 router.get('/users', getUsers);
 router.get('/users/:id', getUserById);
-router.post('/users', createUser);
+router.post('/users/signup', createUser);
 router.patch('/users/:id', updateUser);
 router.delete('/users/:id', deleteUser);
  
